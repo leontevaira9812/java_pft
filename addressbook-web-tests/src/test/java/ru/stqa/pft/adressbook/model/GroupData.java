111e468320f1616963bd66945e85a3c62a6f1377
Irina <leontevaira@inbox.ru>
2023-03-29 19:52:21 +0300

    формат xml

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/model/GroupData.java
@@ -1,106 +1,14 @@
 package ru.stqa.pft.adressbook.model;
 
-/*public record GroupData(int id, String name, String logo, String comment) {
 
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
-  }
-
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "id='" + id + '\'' +
-            ", name='" + name + '\'' +
-            '}';
-
-  }
-
-  public int getId() {
-    return id;
-  }
-
-
-}
-
-
-
-
-/*
-import java.util.Objects;
-
-public class GroupData {
-  private int id;
-  private final String name;
-  private final String logo;
-  private final String comment;
-
-  public GroupData(int id, String name, String logo, String comment) {
-    this.id = id;
-    this.name = name;
-    this.logo = logo;
-    this.comment = comment;
-
-  }
-
-  public String getLogo() {
-    return logo;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getComment() {
-    return comment;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "id=" + id +
-            ", name='" + name + '\'' +
-            '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
-  }
-
-}
-
- */
-
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
 import java.util.Objects;
 
+@XStreamAlias("group")
 public class GroupData {
+  @XStreamOmitField
   public int id = Integer.MAX_VALUE;
   public String name;
   public String logo;
