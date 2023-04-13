5ff3b4a4153ca8799dee0b0463c18270e72e997c
Irina <leontevaira@inbox.ru>
2023-04-13 17:53:19 +0300

    новый проект для нового учебного приложения mantis

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,35 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+import java.io.IOException;
+
+public class TestBase {
+
+  public static final ApplicationManager app;
+
+  static {
+    try {
+      app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() throws Exception {
+    app.stop();
+  }
+
+
+}
