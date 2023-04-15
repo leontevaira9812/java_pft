844ee17be4dd55102625efc1d414d893f45f952d
Irina <leontevaira@inbox.ru>
2023-04-15 22:21:57 +0300

    лекция 10.3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ApplicationManager.java
@@ -66,7 +66,7 @@ public class ApplicationManager {
     } else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7")));
+      //capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7")));
       driver = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
     driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
